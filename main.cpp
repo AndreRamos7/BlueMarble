d8f37bcaa85a6bd4afd920ffea68fa631ddbdcff
André Ramos <andre-rammos@hotmail.com>
2023-11-03 22:23:17 -0300

    Adicionar arquivos de projeto.

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+
+#include <glm/glm.hpp>
+#include <glm/gtx/string_cast.hpp>
+#include <stb_image.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+using namespace std;
+
+
+int main() {
+	glm::vec3 V{ 1,2,3 };
+
+	glfwInit();
+	glewInit();
+
+
+	cout << "Hello world " << glm::to_string(V) << endl;
+	return 0;
+}
